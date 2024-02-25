badb806b45632d7af6e7cc7b3253f1d19bbf88dd
SergeyIvaschennko <boss.awdr@mail.ru>
2024-02-26 00:29:18 +0300

    Add Put, Delete

--- src/main/java/com/example/players_crud/controller/PlayerController.java
+++ src/main/java/com/example/players_crud/controller/PlayerController.java
@@ -1,5 +1,6 @@
 package com.example.players_crud.controller;
 
+import com.example.players_crud.exception.PlayerNotFoundException;
 import com.example.players_crud.model.Player;
 import com.example.players_crud.repository.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class PlayerController {
 
@@ -21,6 +23,32 @@ public class PlayerController {
     @GetMapping("/player")
     List<Player> getAllplayers() {
         return playerRepository.findAll();
+    }
+
+    @GetMapping("/player/{id}")
+    Player getplayerById(@PathVariable Long id) {
+        return playerRepository.findById(id)
+                .orElseThrow(() -> new PlayerNotFoundException(id));
+    }
+
+    @PutMapping("/player/{id}")
+    Player updatePlayer(@RequestBody Player newPlayer, @PathVariable Long id) {
+        return playerRepository.findById(id)
+                .map(player -> {
+                    player.setName(newPlayer.getName());
+                    player.setLastname(newPlayer.getLastname());
+                    player.setAge(newPlayer.getAge());
+                    return playerRepository.save(player);
+                }).orElseThrow(() -> new PlayerNotFoundException(id));
+    }
+
+    @DeleteMapping("/player/{id}")
+    String deletePlayer(@PathVariable Long id){
+        if(!playerRepository.existsById(id)){
+            throw new PlayerNotFoundException(id);
+        }
+        playerRepository.deleteById(id);
+        return  "Player with id "+id+" has been deleted success.";
     }
 
 }
